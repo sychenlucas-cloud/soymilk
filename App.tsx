80cd465347b2aa5c631f6e170cbf0fe3a701d10c
sychenlucas-cloud <sychen.lucas@gmail.com>
2025-12-04 18:32:43 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Navbar from './components/Navbar.tsx;
-import Hero from "./components/Hero.tsx"; 
-import AboutSection from './components/AboutSection.tsx;
-import MenuSection from './components/MenuSection.tsx;
-import ReviewsSection from './components/ReviewsSection.tsx;
-import Locations from './components/Locations.tsx;
-import Footer from './components/Footer.tsx;
-import ChatWidget from './components/ChatWidget.tsx;
+import Navbar from './components/Navbar.tsx'; 
+import Hero from './components/Hero.tsx'; 
+import AboutSection from './components/AboutSection.tsx';
+import MenuSection from './components/MenuSection.tsx';
+import ReviewsSection from './components/ReviewsSection.tsx';
+import Locations from './components/Locations.tsx';
+import Footer from './components/Footer.tsx';
+import ChatWidget from './components/ChatWidget.tsx';
 
 const App: React.FC = () => {
   return (
