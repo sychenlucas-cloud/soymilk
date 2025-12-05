5c80a2cd83ab0c8bef7575ece2cdfd2d8f62b38c
sychenlucas-cloud <sychen.lucas@gmail.com>
2025-12-04 18:19:48 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import Hero from "./components/Hero.tsx"; 
 import AboutSection from './components/AboutSection';
 import MenuSection from './components/MenuSection';
 import ReviewsSection from './components/ReviewsSection';
